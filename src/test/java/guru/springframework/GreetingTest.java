6d7a14bb1f96cc153d02f7e5905d1359bba9ff4a
yul <yu.li@tecsys.com>
2019-03-03 20:56:31 -0500

    Fix typos

---
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -0,0 +1,39 @@
+//: guru.springframework.GreatingTest.java
+
+
+package guru.springframework;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class GreetingTest {
+
+	private Greeting greeting;
+	
+	@BeforeEach
+	void setUp() {
+		System.out.println("In BeforeEach ... ");
+		this.greeting = new Greeting();
+	}
+	
+	@AfterEach
+	void tearDown() {
+		System.out.println("In AfterEach ... ");
+	}
+	
+	@Test
+	void testHelloWorld() {
+		System.out.println(this.greeting.helloWorld());
+	}
+
+	@Test
+	void testHelloWorldString() {
+		System.out.println(this.greeting.helloWorld("Yu"));
+	}
+
+}///:~
