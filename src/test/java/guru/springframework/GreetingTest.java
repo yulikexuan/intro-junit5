2bb6861aaff12cda616eaba6c953568842a4bacb
yul <yu.li@tecsys.com>
2019-03-03 21:03:45 -0500

    Junit5 test-life cycle

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -6,7 +6,9 @@ package guru.springframework;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +16,11 @@ import org.junit.jupiter.api.Test;
 public class GreetingTest {
 
 	private Greeting greeting;
+	
+	@BeforeAll
+	public static void beforeClass() {
+		System.out.println("Before - I am only called once.");
+	}
 	
 	@BeforeEach
 	void setUp() {
@@ -24,6 +31,11 @@ public class GreetingTest {
 	@AfterEach
 	void tearDown() {
 		System.out.println("In AfterEach ... ");
+	}
+	
+	@AfterAll
+	public static void afterClass() {
+		System.out.println("After !!! *** I am only called once. ***");
 	}
 	
 	@Test
